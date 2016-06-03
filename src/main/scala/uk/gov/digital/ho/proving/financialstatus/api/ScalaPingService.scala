fad4a881a01457d824aa7fe63ab71daf27d57d15
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-03 11:52:13 +0100

    [AB] - Sample Scala rest service

---
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/ScalaPingService.scala
@@ -0,0 +1,19 @@
+package uk.gov.digital.ho.proving.financialstatus.api
+
+import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.http.{HttpStatus, ResponseEntity}
+import org.springframework.web.bind.annotation.{ControllerAdvice, RequestMapping, RequestMethod, RestController}
+
+@RestController
+@ControllerAdvice
+class ScalaPingService {
+    @RequestMapping(value = Array("/incomeproving/v1/individual/financialstatus/ping/s"), method = Array(RequestMethod.GET))
+    def ping: ResponseEntity[String] = {
+
+        val LOGGER: Logger = LoggerFactory.getLogger(classOf[ScalaPingService])
+
+        LOGGER.info("Ping received")
+        new ResponseEntity[String]("Pong", HttpStatus.OK)
+    }
+}
+
