3e76194825b534385449c8d180f41caee5fc3ff5
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-07 11:50:35 +0100

    Tidy up task

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/ServiceConfiguration.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/ServiceConfiguration.scala
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter
 import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.springframework.context.annotation.{Bean, Configuration, Primary}
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
@@ -21,15 +22,15 @@ class ServiceConfiguration {
   @Bean
   @Primary
   def objectMapper = {
-    val m = new ObjectMapper()
-    m.registerModule(DefaultScalaModule)
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
 
     val javaTimeModule = new JavaTimeModule()
     javaTimeModule.addDeserializer(classOf[LocalDate], new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-M-d")))
-    m.registerModule(javaTimeModule)
-    m.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    m.enable(SerializationFeature.INDENT_OUTPUT)
-    m
+    mapper.registerModule(javaTimeModule)
+    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    mapper.enable(SerializationFeature.INDENT_OUTPUT)
+    mapper
   }
 
   @Bean
