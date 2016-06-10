3c7e1f74239e95d50faaaadce24986e76e69c23e
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-10 16:07:44 +0100

    Fix test data stream

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/acl/BarclaysBankService.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/acl/BarclaysBankService.scala
@@ -10,8 +10,8 @@ class BarclaysBankService extends BankService {
 
   def fetchAccountDailyBalances(account: Account, endDate: LocalDate, startDate: LocalDate) = {
 
-    lazy val balances: Stream[AccountDailyBalance] = AccountDailyBalance(LocalDate.now(), BigDecimal(scala.util.Random.nextInt(2000) + 500)) #::
-      balances.map { dailyBalance => AccountDailyBalance(dailyBalance.date.minusDays(1), BigDecimal(scala.util.Random.nextInt(2000) + 500)) }
+    lazy val balances: Stream[AccountDailyBalance] = AccountDailyBalance(endDate, BigDecimal(scala.util.Random.nextInt(500) + 2600)) #::
+      balances.map { dailyBalance => AccountDailyBalance(dailyBalance.date.minusDays(1), BigDecimal(scala.util.Random.nextInt(500) + 2600)) }
 
     val applicableBalances = balances.takeWhile(balance => (!balance.date.isBefore(startDate) && !balance.date.isAfter(endDate))).toVector
     applicableBalances
