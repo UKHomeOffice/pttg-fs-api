95a694ee2fbb95bfe0e6ecf9bc03805508f19daa
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-14 11:28:56 +0100

    Fix type issue

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/domain/AccountStatusChecker.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/domain/AccountStatusChecker.scala
@@ -7,7 +7,7 @@ class AccountStatusChecker(bankService: BankService) {
 
   private def areDatesConsecutive(accountDailyBalances: AccountDailyBalances) = {
     val dates = accountDailyBalances.balances.map { _.date }.sortWith((date1, date2) => date1.isBefore(date2))
-    val consecutive = dates.sliding(2).map { case Vector(d1, d2) => d1.plusDays(1).isEqual(d2) }.toVector
+    val consecutive = dates.sliding(2).map { case Seq(d1, d2) => d1.plusDays(1).isEqual(d2) }.toVector
     consecutive.forall(_ == true)
   }
 
