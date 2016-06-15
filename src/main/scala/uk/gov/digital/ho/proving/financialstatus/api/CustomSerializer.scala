b2ac38713b64c44fb5feec3bd385870766d8ccff
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-15 08:08:54 +0100

    Add custome serializer for scala's BIgDecimal

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/CustomSerializer.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/CustomSerializer.scala
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.{JsonSerializer, SerializerProvider}
 class CustomSerializer extends JsonSerializer[BigDecimal] {
 
   override def serialize(value: BigDecimal , jgen: JsonGenerator, provider: SerializerProvider ) {
-    // put your desired money style here
+    // Set the default BigDecimal serialization to 2 decimal places
     jgen.writeString(value.setScale(2, BigDecimal.RoundingMode.HALF_UP).toString())
   }
 
