756717e6b36eeee1b6981e147d24cb5c6cf44aa6
Andy Birkett <andrew.birkett@digital.homeoffice.gov.uk>
2016-06-20 11:28:54 +0100

    Extract duplicate code and manually convert object to json

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/ApiExceptionHandler.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/api/ApiExceptionHandler.scala
@@ -1,44 +1,53 @@
 package uk.gov.digital.ho.proving.financialstatus.api
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpHeaders, HttpStatus, MediaType, ResponseEntity}
-import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.{MissingPathVariableException, MissingServletRequestParameterException}
 import org.springframework.web.bind.annotation.{ControllerAdvice, ExceptionHandler}
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.servlet.NoHandlerFoundException
 
 @ControllerAdvice
-class ApiExceptionHandler {
+class ApiExceptionHandler @Autowired()(objectMapper: ObjectMapper){
+
+  /* TODO
+    For some reason the exception handler is not using the configured ObjectMapper
+    by default, so we have to manually convert the object to string */
 
   private val LOGGER: Logger = LoggerFactory.getLogger(classOf[ApiExceptionHandler])
 
+  private val headers: HttpHeaders = new HttpHeaders
+  headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+
   @ExceptionHandler(Array(classOf[MissingServletRequestParameterException]))
-  def missingParameterHandler(exception: MissingServletRequestParameterException): Any = {
+  def missingParameterHandler(exception: MissingServletRequestParameterException) = {
     LOGGER.debug(exception.getMessage)
-    val headers: HttpHeaders = new HttpHeaders
-    headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
     buildErrorResponse(headers, "0000", "Missing parameter: " + exception.getParameterName, HttpStatus.BAD_REQUEST)
   }
 
   @ExceptionHandler(Array(classOf[NoHandlerFoundException]))
-  def requestHandlingNoHandlerFound(exception: NoHandlerFoundException): Any = {
+  def requestHandlingNoHandlerFound(exception: NoHandlerFoundException) = {
     LOGGER.debug(exception.getMessage)
-    val headers: HttpHeaders = new HttpHeaders
-    headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-    buildErrorResponse(headers, "0000", "Resource not found: " + exception.getRequestURL, HttpStatus.NOT_FOUND)
+    buildErrorResponse(headers, "0000", "Resource not found: Please check the sort code and account number are valid values" + exception.getRequestURL, HttpStatus.NOT_FOUND)
+  }
+
+  @ExceptionHandler(Array(classOf[MissingPathVariableException]))
+  def missingPathVariableException(exception: MissingPathVariableException) = {
+    LOGGER.debug(exception.getMessage)
+    buildErrorResponse(headers, "0000", "Path error: Missing value for " + exception.getParameter, HttpStatus.BAD_REQUEST)
   }
 
   @ExceptionHandler(Array(classOf[MethodArgumentTypeMismatchException]))
-  def methodArgumentTypeMismatchException(exception: MethodArgumentTypeMismatchException): Any = {
+  def methodArgumentTypeMismatchException(exception: MethodArgumentTypeMismatchException) = {
     LOGGER.debug(exception.getMessage)
-    val headers: HttpHeaders = new HttpHeaders
-    headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
     buildErrorResponse(headers, "0000", "Parameter error: Invalid value for " + exception.getName, HttpStatus.BAD_REQUEST)
   }
 
-  private def buildErrorResponse(headers: HttpHeaders, statusCode: String, statusMessage: String, status: HttpStatus)= {
+  private def buildErrorResponse(headers: HttpHeaders, statusCode: String, statusMessage: String, status: HttpStatus) = {
     val response = AccountDailyBalanceStatusResponse(StatusResponse(statusCode, statusMessage))
-    new ResponseEntity(response, headers, status)
+    new ResponseEntity(objectMapper.writeValueAsString(response), headers, status)
   }
 
 }
