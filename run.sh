2711f2132be0c8e24d7ec32a94e42a9c52fc15fa
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-08-23 14:26:03 +0100

    docker scripts and gradle changes for dsp support

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+NAME=${NAME:-pttg-fs-api}
+
+JAR=$(find . -name ${NAME}*.jar|head -1)
+java -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
