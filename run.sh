1f9d32e6231a77c3d4e8cf011e7c9b1be95416f2
lucy.bennett <lucy.bennett@digital.homeoffice.gov.uk>
2016-09-20 10:14:52 +0100

    java_opts trial

--- run.sh
+++ run.sh
@@ -2,6 +2,4 @@
 NAME=${NAME:-pttg-fs-api}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
-echo "Environment java opts:"
-echo ${JAVA_OPTS}
-java -Xms512m -Xmx512m -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
