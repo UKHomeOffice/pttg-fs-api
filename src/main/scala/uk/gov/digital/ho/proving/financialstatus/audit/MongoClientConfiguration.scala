b5415d49ceba6b924fe31fefe927f185f0fa23a4
Tom Ogle <tom.ogle@digital.homeoffice.gov.uk>
2017-02-03 16:18:21 +0000

    Added connection timeout to mongo properties

--- src/main/scala/uk/gov/digital/ho/proving/financialstatus/audit/MongoClientConfiguration.scala
+++ src/main/scala/uk/gov/digital/ho/proving/financialstatus/audit/MongoClientConfiguration.scala
@@ -1,6 +1,8 @@
 package uk.gov.digital.ho.proving.financialstatus.audit
 
 import com.mongodb.MongoClient
+import com.mongodb.MongoClientOptions
+import com.mongodb.ServerAddress
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,12 +10,17 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class MongoClientConfiguration {
 
-  @Value("${auditing.host}") private val auditingHost: String = null
+  @Value("${auditing.host}") private val auditingHost: String = "localhost"
   @Value("${auditing.port}") private val auditingPort: Int = 27017
-
-  // TODO: Production configuration for Mongo client
+  @Value("${auditing.mongo.timeout.ms}") private val auditingMongoTimeoutMs: Int = 30000
 
   @Bean
-  def mongoClient(): MongoClient = new MongoClient(auditingHost, auditingPort)
+  def mongoClient(): MongoClient =
+    new MongoClient(
+      new ServerAddress(auditingHost, auditingPort),
+      MongoClientOptions.builder()
+        .connectTimeout(auditingMongoTimeoutMs)
+        .serverSelectionTimeout(auditingMongoTimeoutMs)
+        .build())
 
 }
